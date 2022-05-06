ee1a9979bdc91625a48143fff64debca933ea6e5
Bhavna Yadav <bhayadav4@publicisgroupe.net>
2022-05-06 15:25:16 +0530

    correction

--- Order/src/main/java/com/demo/order/OrderApplication.java
+++ Order/src/main/java/com/demo/order/OrderApplication.java
@@ -2,12 +2,16 @@ package com.demo.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
 
 @EnableMongoRepositories
 @SpringBootApplication
-@
+@OpenAPIDefinition
+@EnableEurekaClient
 public class OrderApplication {
 
 	public static void main(String[] args) {
