9efaef8fb1b4e0cc08f04aeb1f3dcc665677a8d7
Gondewar Naresh Reddy <gondewarnaresh.reddy@publicissapient.co>
2022-05-06 14:20:01 +0530

    "clodgateway"

--- apigatewayservice/src/main/java/com/apigateway/apigatewayservice/ApigatewayserviceApplication.java
+++ apigatewayservice/src/main/java/com/apigateway/apigatewayservice/ApigatewayserviceApplication.java
@@ -3,7 +3,10 @@ package com.apigateway.apigatewayservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -13,5 +16,13 @@ public class ApigatewayserviceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApigatewayserviceApplication.class, args);
 	}
-
+	@Bean
+	public GlobalFilter globalFilter() {
+		return (exchange, chain) -> {
+			System.out.println("First Global filter");
+			return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+				System.out.println("Second Global filter");
+			}));
+		};
+	}
 }
